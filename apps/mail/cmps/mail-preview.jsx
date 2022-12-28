e680b0ef21cebe332bfd6de761dd7dd485f7a2d9
Yaniv Kela <kianiv94@gmail.com>
2022-12-28 17:55:49 +0200

    added icons when hovering in mail preview

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -7,5 +7,9 @@ export function MailPreview({email}) {
         <td className="username">{email.from.split('@')[0]}</td>
         <td className="email-display"><strong>{email.subject}</strong> - <span>{email.body}</span></td>
         <td className="date">{`${new Date(email.sentAt).getDate()} ${utilService.getMonthName(new Date(email.sentAt)).substring(0,3)}`}</td>
+        <td className="actions">
+            <div className="preview-icon-container"><object className="preview-icon" data="../../../assets/icons/trash.svg" width="15" height="15"></object></div>
+            <div className="preview-icon-container"><object className="preview-icon" data="../../../assets/icons/open-letter.svg" width="25" height="15"></object></div>
+        </td>
     </tr>
 }
