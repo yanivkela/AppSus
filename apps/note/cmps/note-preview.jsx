cf4ea875b072839d1bd7e98f52f0317392609124
Aviv Liberman <liberman809@gmail.com>
2022-12-29 17:59:54 +0200

    add features

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -1,7 +1,7 @@
 
 
 
-export function NotePreview({ note , onDeleteNote , onDuplicatNote }) {
+export function NotePreview({ note, onDeleteNote, onDuplicatNote, onPiningNote}) {
 
 
 
@@ -9,19 +9,26 @@ export function NotePreview({ note , onDeleteNote , onDuplicatNote }) {
 
         <div className="note-contant">
 
-        <div className="top-menu-note">
-            <div className="preview-icon-container" onClick={(ev) => onToggleRead(ev, email)}><object className="preview-icon" data="../../../assets/icons/thumbtack-empty.svg" width="25" height="15"></object></div>
-        </div>
+            <div className="top-menu-note">
+                <div className="preview-icon-container" onClick={(ev) => onPiningNote(note.id)}><object className="preview-icon" data="../../../assets/icons/thumbtack-empty.svg" width="25" height="15"></object></div>
+            </div>
 
             {note.info.txt}
 
         </div>
 
-        <div className="bottom-menu-note">
-            <div className="preview-icon-container" onClick={(ev) => onDeleteNote(note.id)}><object className="preview-icon" data="../../../assets/icons/trash.svg" width="25" height="15"></object></div>
-            <div className="preview-icon-container" onClick={(ev) => onToggleRead(ev, email)}><object className="preview-icon" data="../../../assets/icons/paint-pallet-svgrepo-com.svg" width="25" height="15"></object></div>
-            <div className="preview-icon-container" onClick={(ev) => onDuplicatNote(note)}><object className="preview-icon" data="../../../assets/icons/copies-svgrepo-com.svg" width="25" height="15"></object></div>
+        <div className="note-footer">
+            
+            <div className="bottom-menu-note">
+                <div className="preview-icon-container" onClick={(ev) => onDeleteNote(note.id)}><object className="preview-icon" data="../../../assets/icons/trash.svg" width="25" height="15"></object></div>
+                <div className="preview-icon-container color" onClick={(ev) => onToggleRead(ev, email)}><object className="preview-icon" data="../../../assets/icons/paint-pallet-svgrepo-com.svg" width="25" height="15"></object></div>
+                <div className="preview-icon-container" onClick={(ev) => onDuplicatNote(note)}><object className="preview-icon" data="../../../assets/icons/copies-svgrepo-com.svg" width="25" height="15"></object></div>
+            </div>
         </div>
+
+        {/* <div className="color choice">rewrew</div> */}
+
+
 
     </article>
 }
